b3589c6565cfdca42846b1ae3dc9978dd09d9739
Christian <dynexanimations21@gmail.com>
2025-02-11 19:21:28 -0500

    a
    a

--- src/main/java/partA/ex01/CheckingAccount.java
+++ src/main/java/partA/ex01/CheckingAccount.java
@@ -1,6 +1,25 @@
 package partA.ex01;
 
 public class CheckingAccount {
+    public String name;
+    public Double balance;
 
+    public void deposit(double amount){
+        balance += amount;
+    }
+
+    public static void main(String[] args){
+        CheckingAccount user1 = new CheckingAccount();
+        user1.balance = 231.0;
+        user1.name = "John";
+
+        System.out.println(user1.balance);
+
+        user1.deposit(212.2);
+
+        System.out.println(user1.balance);
+        System.out.println(user1.name);
+            }
 
 }
+
--- src/main/java/partA/ex02/SavingBond.java
+++ src/main/java/partA/ex02/SavingBond.java
@@ -1,4 +1,63 @@
 package partA.ex02;
 
 public class SavingBond {
+    /*
+    A person may purchase a bond for any term between 1 and 60 months
+    A bond earns interest every month until its term matures (0 months remaining)
+    The term and interest rate are set at the same time
+    The bond's interest rate is based on its term according to the following tier system.
+    */
+    public String name;
+    public double balance, interest;
+    public int term, monthLeft;
+
+    public double setInterest(){
+        if (term <= 60 && term >= 48){
+            return 2.5;
+        }
+        else if (term <= 47 && term >= 36){
+            return 2.0;
+        }
+        else if (term <= 35 && term >= 24){
+            return 1.5;
+        }
+        else if (term <= 23 && term >= 12){
+            return 1.5;
+        }
+        return 0.5;
+    }
+    public double earnInterest(){
+        return balance * interest / 12;
+    }
+    public void mature(){
+
+    }
+
+    public static void main(String[] args){
+        SavingBond account = new SavingBond();
+        account.name = "Jimmy";
+        account.balance = 152.6;
+        account.term = 60;
+        account.monthLeft = 0;
+        account.interest = account.setInterest();
+
+        //Before stuff
+        System.out.println("Before");
+        System.out.println(account.name);
+        System.out.println(account.balance);
+        System.out.println(account.term);
+        System.out.println(account.monthLeft);
+        System.out.println(account.interest);
+
+        account.balance += account.earnInterest();
+
+        //After stuff
+        System.out.println("\nAfter");
+        System.out.println(account.name);
+        System.out.println(account.balance);
+        System.out.println(account.term);
+        System.out.println(account.monthLeft);
+        System.out.println(account.interest);
+
+    }
 }
